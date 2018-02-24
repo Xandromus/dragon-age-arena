46a50a5c4cca37792d72a90cef7c5d134a824b6f
Xandromus <xanderrapstine@gmail.com>
2018-02-23 21:16:21 -0600

    added some efficiency to js and commented code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,15 @@
  $(document).ready(function() {
 
+     // global variable declarations
      var yourFighter;
      var defender;
      var fighterSelected;
      var defenderSelected;
      var counter;
      var kills;
+     var reset;
 
+     // object containing characters and their attributes
      var characters = {
          'morrigan': {
              name: 'Morrigan',
@@ -38,117 +41,163 @@
          }
      };
 
+     // function to initialize game
      function initGame() {
 
+         // scroll page to top of screen
          $("body,html").animate({ scrollTop: 0 });
 
+         // set variables to initial state
          fighterSelected = false;
          defenderSelected = false;
          counter = 1;
          kills = 0;
+         reset = $("<button class='reset-button'>").text("Reset");
 
-         $(".fighter-stage, .defender-stage, .player-stable, .attack-reset, .damage-display").empty();
-
+         // create player modules in the stable
          Object.keys(characters).forEach(key => {
-
              var playerStable = $(".player-stable");
              var playerModule = $("<div class='player-module'>");
+
+             // add data attribute with the key name for each character
              playerModule.attr("data-key", key);
+
+             // create corresponding image, name, and health for each character and give health an id unique to the character
              var playerImage = $("<img class='character'>").attr("src", characters[key].image);
              var playerName = $("<h3 class='player-text'>").text(characters[key].name);
              var playerHealth = $("<p class='player-health'>").attr("id", key).text(characters[key].health);
+
+             // add characteristics to player module and place each module in the stable
              playerModule.append(playerImage).append(playerName).append(playerHealth);
              playerStable.append(playerModule);
-
          });
      }
 
+     // click function for player modules that are still in the stable
      $(document).on("click", ".player-stable .player-module", function() {
 
+         // if the defender hasn't been selected, allow the fighter and defender to be selected
          if (defenderSelected === false) {
 
-
+             // if the fighter has been selected, allow the defender to be selected
              if (fighterSelected) {
 
+                 // move the defender's player module from the player stable to the defender stage
                  defender = $(this);
-
                  $(".defender-stage").append(defender);
+
+                 // create attack button
                  var attack = $("<button class='attack-button'>").text("Attack!");
+                 $(".attack-reset").append(attack);
+
+                 // update the instructions
                  $(".instructions").text("Enter battle!");
+
+                 // clear the damage display between battles
                  $(".damage-display").empty();
+                 
+                 // update the outcome display to the initial message
+                 $(".outcome-display").text("fight!");
+
+                 // mark defender as selected
                  defenderSelected = true;
 
+                 // move the screen down so that users can access and view the necessary information
                  $("body,html").animate({ scrollTop: 200 }, 400);
 
-                 $(".attack-reset").append(attack);
-                 $(".outcome-display").text("vs.");
-
              } else {
+
+                // allow the fighter to be selected
                  yourFighter = $(this);
 
+                 // move the fighter's player module from the player stable to the fighter stage
                  $(".fighter-stage").append(yourFighter);
+
+                 // prompt the user to select their opponent
                  $(".instructions").text("Choose your opponent:");
+
+                 // mark fighter as selected
                  fighterSelected = true;
              }
-
          }
-
      });
 
-
-
+     // click function for attack functionality
      $(document).on("click", ".attack-button", function() {
+
+         // create variables to access fighter's key and attributes
          var fighterName = yourFighter.attr("data-key");
          var fighterAttack = characters[yourFighter.attr("data-key")].attack;
+
+         // create variables to access defender's key and attributes
          var defenderName = defender.attr("data-key");
          var defenderAttack = characters[defender.attr("data-key")].defenderCounterAttack;
+
+         // create variables for damage display messages
          var fighterMessage = ("You attacked " + characters[defender.attr("data-key")].name + " for " + (fighterAttack * counter) + " points damage.");
          var defenderMessage = (characters[defender.attr("data-key")].name + " attacked you for " + defenderAttack + " points damage.");
 
+         // decrease defender's health by attack power, increase attack power with each attack, and add the fighter's damage display
          characters[defender.attr("data-key")].health -= (fighterAttack * counter);
          $(".damage-display").append("<p>").text(fighterMessage);
 
+         // once the defender's health reaches zero, allow a new defender to be selected and reset the defender stage. Remove the attack button and update the outcome display and instructions
          if (characters[defender.attr("data-key")].health < 1) {
              defenderSelected = false;
              $(".defender-stage, .attack-reset").empty();
              $(".outcome-display").text("victory");
              $(".instructions").text("Choose your next opponent:");
+
+             // update the kill counter by one and scroll page to the top of the screen
              kills++;
              $("body,html").animate({ scrollTop: 0 }, 2500);
-
          } else {
+             // decrease fighter's health by counter attack power, update health display for both opponents, and add damage display for both players
              characters[yourFighter.attr("data-key")].health -= defenderAttack;
              $("#" + fighterName).text(characters[yourFighter.attr("data-key")].health);
              $("#" + defenderName).text(characters[defender.attr("data-key")].health);
              $(".damage-display").append("<p>").html("" + fighterMessage + "<br /><br />" + defenderMessage + "");
-
          }
+
+         // inscrease the counter by one to increase the fighter's attack power on each click
          counter++;
 
+         // if fighter's health reaches zero, user has lost the game
          if (characters[yourFighter.attr("data-key")].health < 1) {
+
+             // set fighter's health to zero to avoic negative numbers in display
              $("#" + fighterName).text(0);
+
+             // update instructions and outcome and prompt player to reset game
              $(".instructions").text("Defeated! Press reset to try again!");
              $(".outcome-display").text("defeat");
+
+             // get rid of attack button and clear damage display
              $(".attack-reset, .damage-display").empty();
-             var reset = $("<button class='reset-button'>").text("Reset");
+
+             // create reset button
              $(".attack-reset").append(reset);
              $("body,html").animate({ scrollTop: 0 }, 2500);
          }
 
+         // the user wins when they have defeated all 3 of the other opponents
          if (kills === 3) {
+
+             // clear the damage display and update the instructions and outcome
              $(".damage-display").empty();
              $(".instructions").text("Champion! Press reset to play again!");
              $(".outcome-display").text("champion");
-             var reset = $("<button class='reset-button'>").text("Reset");
+             
+             // create reset button
              $(".attack-reset").append(reset);
          }
-
      });
 
+     // reload page when user clicks reset button
      $(document).on("click", ".reset-button", function() {
          location.reload(true);
      });
 
-
+     // call function to initialize the game
      initGame();
  });
